8ecee73cdefc1f87a9cb2a8257f43faf57093ba2
Keisuke SATO <riafweb@gmail.com>
2009-08-13 19:22:19 +0900

    なんだこれすげえw

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 require './__settings__.php';
 import('Status');
 
-$flow = new Flow();
-$flow->handler(array(
+R('Flow')->handler(array(
     '^$' => 'class=Status,method=models,name=status,template=statuses.html',
     '^/status/(\d+)$' => 'class=Status,method=model,name=status,template=statuses_detail.html',
     '^/update$' => 'class=Status,method=create,name=status',
