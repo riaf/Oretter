132cc46136a7f0be47fcc8a43d472bce34afd246
Keisuke SATO <riafweb@gmail.com>
2009-08-14 20:38:37 +0900

    good bye

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require './__settings__.php';
-
-R(Flow)->handler(array(
-    '^$' => 'class=Status,method=models,name=status,template=statuses.html',
-    '^/status/(\d+)$' => 'class=Status,method=model,name=status,template=statuses_detail.html',
-    '^/update$' => 'class=Status,method=create,name=status',
-))->output();
